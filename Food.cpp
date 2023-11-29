46e7bffb2246d4a9d7331fe0b4996743be10fca4
lentzfortune <117185451+lentzfortune@users.noreply.github.com>
2023-11-29 13:11:22 -0500

    Updated food files
    I overloaded "<<" and ">>" operators, so Elijah can easily read objects of type Food into his files. Added a switch case. Elijah will have to update file names.

--- Food.cpp
+++ Food.cpp
@@ -1,70 +1,83 @@
 #include <iostream>
 #include <string>
+#include "Food.h"
+#include <fstream>
 using namespace std;
 
+Food chickenAlfredo{1570,"Chicken Alfredo"};
+Food spaghettiAndMeatballs{680,"Spaghetti and Meatballs"};
+Food tourOfItaly{1550,"Tour of Italy"};
+// Appitizers
+Food friedMozzarella{800,"Fried Mozzarella"};
+Food stuffedZifiFritta{500,"Stuffed Ziti Fritta"};
+// Drinks
+Food raspberryLemondade{170,"Raspberry Lemonade"};
+Food classicLemonade{170,"Classic Lemonda"};
+// Desert
+Food blackTieMousseCake{750,"Black Mouse Tie"};
+Food strawberryCreamCake{470,"Strawberry Cream Cake"};
+void foodSelect();
+void displayMenu();
+ 
 
+void displayMenu(){
+   cout<< "1. Chicken Alfredo";
+}
+// Funtion for user entering food.
+void foodSelect(){
+int userChoice=0;
+ofstream sampleCalorie;
+sampleCalorie.open("Sample.txt",std::ios::app);// open txt file in this line. Needs to be replaced
+sampleCalorie<<__DATE__<<" "<<__TIME__<<endl;// outputs time everytime the file is open
+Food total {0,"Total"};// variable that holds total amount of calories
+do{
+cout<< "Enter choice ";// switch case for users choice
+cin>> userChoice;
+    switch(userChoice){
+         case 1:
+        total+=tourOfItaly;
+        sampleCalorie<<tourOfItaly;// In each situation the name and calorie is sent out to a file in a certain format. Refer to Food.h file for format.
+        case 2:
+        total+=chickenAlfredo;
+        sampleCalorie<<chickenAlfredo;
+        break;
+        case 3:
+        total+=spaghettiAndMeatballs;
+        sampleCalorie<<spaghettiAndMeatballs;
+        break;
+        case 4:
+        total+=friedMozzarella;
+        sampleCalorie<<friedMozzarella;
+        break;
+        case 5:
+        total+=stuffedZifiFritta;
+        sampleCalorie<<friedMozzarella;
+        break;
+        case 6:
+        total+=raspberryLemondade;
+        sampleCalorie<<raspberryLemondade;
+        break;
+        case 7:
+        total+=classicLemonade;
+        sampleCalorie<<classicLemonade;
+        break;
+        case 8:
+        total+=blackTieMousseCake;
+       sampleCalorie<<blackTieMousseCake;      
+        break;
+        case 9:
+        total+=strawberryCreamCake;
+        sampleCalorie<<strawberryCreamCake;
+        break;
+        case 10: break;
+        default:
+            cout<<"Not an option"<<endl;}
 
-class Food{
-public:
-    Food(int number=0, int calorie=0, string food= "Food"){
-        if (number >= 0 && calorie >= 0){
-            itemNumber = number;
-            caloriePerServing = calorie;
-        }
-        foodName = food;
-    }
-    // Food class
-
-void setFood(){
-    cout<<"Enter food name: \n";
-    cin>> foodName;
-    cout<<"Enter amount of calories: \n";
-    cin>> caloriePerServing;
-    cout<<"Enter item number \n";
-    cin>> itemNumber;  
-}//used for costum food items
-void getFood(){
-    cout<< foodName<<": "<<caloriePerServing;
-} // getter for food
-
-private:
-int itemNumber;
-int caloriePerServing;
-string foodName;
-};
-
-int main(){
-// entrees
-Food chickenAlfredo{1,1570,"Chicken Alfredo"};
-Food spaghettiAndMeatballs{2,680,"Spaghetti and Meatballs"};
-Food fettuccineAlfredo{3,650,"Fettuccine Alfredo"};
-Food chickenParm{4,630,"Chicken Parmigiana"};
-Food cheeseRavioli{5,500,"Cheese Ravioli"};
-Food grilledSalmon{6,610,"Grilled Salmon"};
-Food eggplantParmigiana{19,660,"Eggplant Parmigiana"};
-Food shrimpScampi{20,460,"Shrimp Scampi"};
-// Appetizers
-Food friedMozzarella{7,800,"Fried Mozzarella"};
-Food stuffedZifiFritta{8,500,"Stuffed Ziti Fritta"};
-Food calamari{9,670,"Calamari"};
-Food meatballs{10,1040,"Meatballs"};
-Food lasagnaFritta{11,1130,"Lasagna Fritta"};
-Food toastedRavioli{12,650,"Toasted RAvioli"};
-Food breadStickes{13,140,"Breadsticks (v)"};
-//drinks
-Food raspberryLemondade{14,170,"Raspberry Lemonade"};
-Food classicLemonade{15,170,"Classic Lemonda"};
-Food mangoStrawberryIcedTea{16,100,"Mango-Strawberry Iced Tea"};
-Food soda{17,790,"Coke"};
-Food strawberryPassionFruitlemonda{18,180,"Strawberry-Passion Fruit lemonda"};
-Food Cappuccino{21,150,"Cappuccino"};
-Food millerLite{22,100,"Miller Lite"};
-//deserts
-Food blackTieMousseCake{23,750,"Black Mouse Tie"};
-Food strawberryCreamCake{24,470,"Strawberry Cream Cake"};
-Food costum{25,};
-
-
-
-return 0;
+        
+}
+    while(userChoice != 10);
+    
+    sampleCalorie<< total<<endl; // total being sent out to file. Needs to be changed
+    sampleCalorie.close();
+    
 }
